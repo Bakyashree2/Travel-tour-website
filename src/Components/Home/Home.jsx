2288041b842beed0f2e4c9de7959024368cb4264
Bakyashree2 <148343190+Bakyashree2@users.noreply.github.com>
2025-03-12 22:51:26 +0530

    update the navbar

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Home.scss";
 import { GrLocation} from "react-icons/gr";
+import {HiFilter} from"react-icons/hi";
+import { FiFacebook } from "react-icons/fi";
+import { FaInstagram  } from "react-icons/fi";
+import { FiFacebook } from "react-icons/fi";
 
 
 const Home = () => {
@@ -31,7 +35,68 @@ const Home = () => {
                         <GrLocation className="icon"/>
                    </div>
                </div>
- </div>
+        
+               
+               <div className="dateInput">
+                   <label  htmlFor="date">select you date:</label>
+                   <div className="input flex">
+                       <input type="date"/>
+                   </div>
+               </div>
+
+               <div className="priceInput">
+                   <div className="label_total flex">
+                     <label htmlFor="price">Max price:</label>
+                     <h3 className="total">$5000</h3>  
+                   </div>
+                   <div className="input flex">
+                      <input type="range" max="5000" min="1000"/>
+                   </div>
+               </div>
+
+
+               <div className="searchOptions flex">
+                <HiFilter className="icon"/>
+                <span>MORE FILTERS</span>
+                 </div>
+
+               <div className="homefooterIcons flex">
+                  <div className="rightIcons">
+
+               </div>
+
+               <div className="homeFooterIcons flex">
+                <div className="rightIcons">
+                  <FiFacebook className="icons"/>
+                  <FaInstagram  className="icons"/>
+                  <FaCcStripe className="icons"/>
+            
+            </div>
+               
+                 <div className="leftIcons">
+                  <BsListtask className="icon"/>
+                  <BsListtask className="icon"/>
+
+
+
+
+               </div>
+
+
+
+
+
+            </div>
+
+              </div>
+
+            
+
+
+
+
+
+
     </div>
          
     </section>
